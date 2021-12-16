6f4a404ecc179c94db0797d074c3aee7da5b226c
Benjamin Ran <beni@pinecone.io>
2021-12-16 16:52:22 +0200

    Whitespace fix

--- examples/app.go
+++ examples/app.go
@@ -27,7 +27,7 @@ func main() {
 
 	config := &tls.Config{}
 
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 5 * time.Second)
 	defer cancel()
 
 	ctx = metadata.AppendToOutgoingContext(ctx, "api-key", *apiKey)
